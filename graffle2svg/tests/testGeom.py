7808b4dd2f6429c06774ca73d87f994cf91f97c0
Jean-Noël Avila <jn.avila@free.fr>
2010-06-05 23:27:24 +0200

    Added first rotate tests

--- graffle2svg/tests/testGeom.py
+++ graffle2svg/tests/testGeom.py
@@ -22,7 +22,33 @@ class TestCentre(TestCase):
         assert centre[0] == -2
         assert centre[1] == 0
 
+class TestRotate(TestCase):
+    def testIdentic(self):
+        pts=((-1., 1.), (1., 1.))
+        rotated =geom.rotate_points(pts)
+        assert rotated==((-1.0, 1.0), (1.0, 1.0))
+
+    def testOpposite(self):
+        pts=((-1., 1.), (1., 1.))
+        rotated =geom.rotate_points(pts, angle=180.)
+        assert rotated==[[1., 1.], [-1., 1.]]
+ 
+    def test360(self):
+        pts=((-1., 1.), (1., 1.))
+        rotated =geom.rotate_points(pts, 360)
+        assert rotated==((-1.0, 1.0), (1.0, 1.0))
+        
+    def test90(self):
+        pts=((-1., 1.), (1., 1.))
+        rotated =geom.rotate_points(pts,90)
+        self.assertAlmostEqual(0,rotated[0][0],4)
+        self.assertAlmostEqual(0,rotated[0][1],4)
+        self.assertAlmostEqual(0,rotated[1][0],4)
+        self.assertAlmostEqual(2,rotated[1][1],4)
+        
+      
 def get_tests():
     TS = TestSuite()
     TS.addTest(makeSuite(TestCentre))
+    TS.addTest(makeSuite(TestRotate))
     return TS
